3c9aad6e9ae5c1649a3fba957f656331e6ff0997
Kobir053 <kobir05333@gmail.com>
2024-10-07 22:08:40 +0300

    added middleware method for the student authentication

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -23,11 +23,40 @@ export function teacherMiddleWare(req, res, next) {
             console.log(decoded);
             const teacher = yield userModel.findById(decoded.id);
             if (!teacher) {
-                res.status(404).json({ message: "the token you have shows that you no longer loged in" });
+                res.status(404).json({ message: "the token you have shows that you no longer register" });
                 return;
             }
             if (teacher.role !== "teacher") {
                 res.status(403).json({ message: "you are not a teacher so you don't have access to the student's details" });
+                return;
+            }
+            next();
+        }
+        catch (error) {
+            res.status(403).json({ message: "Invalid token, the error is: " + error.message });
+        }
+    });
+}
+export function studentMiddleware(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!req.cookies["token"]) {
+            res.status(401).json({ message: "you don't have a token cookie!" });
+            return;
+        }
+        if (!req.body.id) {
+            res.status(400).json({ message: "you have to enter your id" });
+            return;
+        }
+        try {
+            const studentToken = req.cookies["token"];
+            let decoded = jwt.verify(studentToken, JWT_SECRET);
+            const student = yield userModel.findById(decoded.id);
+            if (!student) {
+                res.status(404).json({ message: "the token you have shows that you no longer register" });
+                return;
+            }
+            if (student._id !== req.body.id) {
+                res.status(403).json({ message: "you don't have access to another students account!" });
                 return;
             }
             next();
--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -18,12 +18,46 @@ export async function teacherMiddleWare (req: Request, res: Response, next: Next
         const teacher = await userModel.findById(decoded.id);
 
         if(!teacher){
-            res.status(404).json({message: "the token you have shows that you no longer loged in"});
+            res.status(404).json({message: "the token you have shows that you no longer register"});
             return;
         }
 
         if(teacher.role !== "teacher"){
             res.status(403).json({message: "you are not a teacher so you don't have access to the student's details"});
+            return;
+        }
+
+        next();
+    } 
+    catch (error: any) {
+        res.status(403).json({message:"Invalid token, the error is: " + error.message});
+    }
+}
+
+export async function studentMiddleware (req: Request, res: Response, next: NextFunction) {
+
+    if(!req.cookies["token"]){
+        res.status(401).json({message: "you don't have a token cookie!"});
+        return;
+    }
+
+    if(!req.body.id){
+        res.status(400).json({message: "you have to enter your id"});
+        return;
+    }
+    
+    try {
+        const studentToken = req.cookies["token"];
+        let decoded = jwt.verify(studentToken, JWT_SECRET) as {id: string, iat: number, exp: number};
+        const student = await userModel.findById(decoded.id);
+
+        if(!student){
+            res.status(404).json({message: "the token you have shows that you no longer register"});
+            return;
+        }
+
+        if(student._id !== req.body.id){
+            res.status(403).json({message: "you don't have access to another students account!"});
             return;
         }
 
